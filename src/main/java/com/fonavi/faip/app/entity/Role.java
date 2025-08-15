497c96f70c4542184fb58c648f0025fc4eb40097
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-08-14 23:54:48 -0500

    feat(Role): Entidad

---
+++ src/main/java/com/fonavi/faip/app/entity/Role.java
@@ -0,0 +1,21 @@
+package com.fonavi.faip.app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String nombre;
+}
