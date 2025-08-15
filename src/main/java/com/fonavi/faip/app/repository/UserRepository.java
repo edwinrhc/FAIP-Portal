fecd1c76467eb45f8b85b12addeae444400da2b0
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-08-14 23:59:20 -0500

    feat(UserRepository): Crear Método buscar Por usuario

---
+++ src/main/java/com/fonavi/faip/app/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.fonavi.faip.app.repository;
+
+import com.fonavi.faip.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
