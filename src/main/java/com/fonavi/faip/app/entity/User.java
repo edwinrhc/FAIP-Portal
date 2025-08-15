d4c716bfdf4c332fedee084d1cadc07997d6410d
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-08-14 23:57:33 -0500

    feat(User): Crear entidad

---
+++ src/main/java/com/fonavi/faip/app/entity/User.java
@@ -0,0 +1,33 @@
+package com.fonavi.faip.app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_roles",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Role> roles;
+
+}
