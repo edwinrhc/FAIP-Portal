bf24f152198e5af37b61526f09d28095a028e2ee
Edwin HC <edwinrhc@gmail.com>
2025-08-22 19:03:16 -0500

    feat(security): enable CORS configuration to allow requests from Angular frontend

--- src/main/java/com/fonavi/faip/app/security/SecurityConfig.java
+++ src/main/java/com/fonavi/faip/app/security/SecurityConfig.java
@@ -21,7 +21,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(
+                csrf -> csrf.disable())
+                .cors(cors -> {})
                 .authorizeHttpRequests( auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers(
